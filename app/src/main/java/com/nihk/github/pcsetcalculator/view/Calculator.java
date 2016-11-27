9379b17bbe9a568d59cda7695bbf52a989fff201
nihk <nickjrose@gmail.com>
2016-11-26 19:49:21 -0500

    Add layout for output

--- app/src/main/java/com/nihk/github/pcsetcalculator/view/Calculator.java
+++ app/src/main/java/com/nihk/github/pcsetcalculator/view/Calculator.java
@@ -11,12 +11,16 @@ import com.nihk.github.pcsetcalculator.R;
  * Created by Nick on 2016-11-26.
  */
 
-public class InputNumbersRow extends TableLayout {
-    public InputNumbersRow(Context context, AttributeSet attrs) {
+public class Calculator extends TableLayout {
+    private CalculatorButtons mCalculatorButtons;
+
+    public Calculator(Context context, AttributeSet attrs) {
         super(context, attrs);
 
-        LayoutInflater.from(context).inflate(R.layout.numbers_input_table, this, true);
+        LayoutInflater.from(context).inflate(R.layout.calculator, this, true);
 
         // TODO make buttons clickable to have a sound. Make them simultaneously sound
+        // TODO pressing Tn button will light all the numbered buttons (+ itself) up blue, In
+        // will light them up (+ itself) red
     }
 }
---
+++ app/src/main/java/com/nihk/github/pcsetcalculator/view/CalculatorButtons.java
@@ -0,0 +1,12 @@
+package com.nihk.github.pcsetcalculator.view;
+
+import android.widget.Button;
+
+import java.util.List;
+
+/**
+ * Container class for the rows of buttons on the calculator.
+ */
+public final class CalculatorButtons {
+    private List<Button> mButtons;
+}
