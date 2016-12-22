5c1b98e8284ae826fd6f1c5e9cf3e2dc19d72bfe
nihk <nickjrose@gmail.com>
2016-12-21 22:04:00 -0500

    Refactored RahnForteUtils a bit to purge a few code duplications

--- app/src/main/java/com/nihk/github/pcsetcalculator/utils/RahnForteUtils.java
+++ app/src/main/java/com/nihk/github/pcsetcalculator/utils/RahnForteUtils.java
@@ -8,41 +8,67 @@ import com.nihk.github.pcsetcalculator.models.PitchClassSet;
 import java.util.HashMap;
 import java.util.Map;
 
+import static com.nihk.github.pcsetcalculator.utils.ForteNumberUtils.*;
+
 /**
  * Created by Nick on 2016-12-17.
  */
 
+/**
+ * A utility class for assisting with the Rahn and Forte primes which don't match due
+ * to different algorithm usage. Since only 6 set classes differ (and that amount is not
+ * subject to change) it's easiest to just handle it on a case-by-case basis.
+ */
 public final class RahnForteUtils {
+    private static final int RAHN_5_20;
+    private static final int RAHN_6_Z29;
+    private static final int RAHN_6_31;
+    private static final int RAHN_7_Z18;
+    private static final int RAHN_7_20;
+    private static final int RAHN_8_26;
+
+    private static final int FORTE_5_20 =   395;
+    private static final int FORTE_6_Z29 =  843;
+    private static final int FORTE_6_31 =   811;
+    private static final int FORTE_7_Z18 =  815;
+    private static final int FORTE_7_20 =   919;
+    private static final int FORTE_8_26 =   1719;
+
     private RahnForteUtils() {
         // Prevent instantiation
+    }
+
+    public static final BiMap<Integer, Integer> RAHN_TO_FORTE_PRIMES_BINARY = HashBiMap.create();
+
+    static {
+        final BiMap<ForteNumber, Integer> inverseBiMap = BIMAP.inverse();
+        RAHN_5_20 =    inverseBiMap.get(_5_20);
+        RAHN_6_Z29 =   inverseBiMap.get(_6_Z29);
+        RAHN_6_31 =    inverseBiMap.get(_6_31);
+        RAHN_7_Z18 =   inverseBiMap.get(_7_Z18);
+        RAHN_7_20 =    inverseBiMap.get(_7_20);
+        RAHN_8_26 =    inverseBiMap.get(_8_26);
+
+        RAHN_TO_FORTE_PRIMES_BINARY.put(RAHN_5_20,  FORTE_5_20);
+        RAHN_TO_FORTE_PRIMES_BINARY.put(RAHN_6_Z29, FORTE_6_Z29);
+        RAHN_TO_FORTE_PRIMES_BINARY.put(RAHN_6_31,  FORTE_6_31);
+        RAHN_TO_FORTE_PRIMES_BINARY.put(RAHN_7_Z18, FORTE_7_Z18);
+        RAHN_TO_FORTE_PRIMES_BINARY.put(RAHN_7_20,  FORTE_7_20);
+        RAHN_TO_FORTE_PRIMES_BINARY.put(RAHN_8_26,  FORTE_8_26);
     }
 
     public static boolean isForteRahnUnequalPrime(int set) {
         return RAHN_TO_FORTE_PRIMES_BINARY.containsKey(set);
     }
 
-    // These Rahn prime forms are not the same prime forms when using the Forte
-    // algorithm. Since it only affects 5 set classes, I used a Map instead of implementing
-    // the different algorithm in its entirety.
-    public static final BiMap<Integer, Integer> RAHN_TO_FORTE_PRIMES_BINARY = HashBiMap.create();
-
-    static {
-        RAHN_TO_FORTE_PRIMES_BINARY.put(355, 395);      // 5-20
-        RAHN_TO_FORTE_PRIMES_BINARY.put(717, 843);      // 6-Z29
-        RAHN_TO_FORTE_PRIMES_BINARY.put(691, 811);      // 6-31
-        RAHN_TO_FORTE_PRIMES_BINARY.put(755, 815);      // 7-Z18
-        RAHN_TO_FORTE_PRIMES_BINARY.put(743, 919);      // 7-20
-        RAHN_TO_FORTE_PRIMES_BINARY.put(1467, 1719);    // 8-26
-    }
-
     // Inversions of the Forte primes, zero based. This is used for getting normal form using the Forte algorithm
     public static final Map<Integer, Integer> FORTE_PRIME_INVERSIONS = new HashMap<Integer, Integer>() {{
-        put(395, 419);      // 5-20
-        put(843, 843);      // 6-Z29
-        put(811, 851);      // 6-31
-        put(815, 979);      // 7-Z18
-        put(919, 935);      // 7-20
-        put(1719, 1719);    // 8-26
+        put(FORTE_5_20,     419);
+        put(FORTE_6_Z29,    843);
+        put(FORTE_6_31,     851);
+        put(FORTE_7_Z18,    979);
+        put(FORTE_7_20,     935);
+        put(FORTE_8_26,     1719);
     }};
 
     public static boolean isPrimeFormDifferentDependingOnAlgorithm(final String forteNumber) {
@@ -61,6 +87,7 @@ public final class RahnForteUtils {
         if (pitchClassSet == null) {
             return false;
         }
+
         final ForteNumber forteNumber = pitchClassSet.getForteNumber();
 
         if (forteNumber == null) {
